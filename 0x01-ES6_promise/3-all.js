15df3ed0459ec8fe6e5645478d9478c6df227617
Eloagbawe <eloagbawe@gmail.com>
2023-01-26 00:01:43 +0100

    update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((data) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((data) => {
     const res = Object.assign({}, ...data);
     console.log(res.body, res.firstName, res.lastName);
   }).catch(() => { console.log('Signup system offline'); });
