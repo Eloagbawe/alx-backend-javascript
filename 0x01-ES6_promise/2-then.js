4cae9083b756846af06051de682c57f485e22895
Eloagbawe <eloagbawe@gmail.com>
2023-01-25 03:29:34 +0100

    update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -12,7 +12,7 @@ export default function handleResponseFromAPI(promise) {
       status: 200,
       body: 'success',
     }),
-    () => new Error(),
+    () => new Error({}),
     console.log('Got a response from the API'),
   );
 }
