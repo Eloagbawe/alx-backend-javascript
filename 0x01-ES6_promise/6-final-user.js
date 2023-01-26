b0986a517f04224a0484f260821b0d4947c8413a
Eloagbawe <eloagbawe@gmail.com>
2023-01-26 01:13:39 +0100

    update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  const result = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((data) => data);
-  return result;
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((data) => {
+      const res = [];
+
+      for (let i = 0; i < data.length; i += 1) {
+        res.push({
+          status: data[i].status,
+          value: data[i].value ? data[i].value : data[i].reason,
+        });
+      }
+      return res;
+    });
 }
