17ad5f0a7228fbc98852c98a25bc7dab4bfa8c3d
Eloagbawe <eloagbawe@gmail.com>
2023-01-26 23:05:19 +0100

    add solution for task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof (length) === 'number') {
+      this._length = length;
+    } else {
+      throw TypeError('Length must be a number');
+    }
+
+    if (Array.isArray(students) && students.every((i) => typeof i === 'string')) {
+      this._students = students;
+    } else {
+      throw TypeError('Students must be an array of strings');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+}
