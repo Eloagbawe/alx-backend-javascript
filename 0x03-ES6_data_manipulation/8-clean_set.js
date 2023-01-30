e33471c5beedaadcb24b4ed4400d047f80ff2e95
Eloagbawe <eloagbawe@gmail.com>
2023-01-30 20:30:43 +0100

    update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(set, startString) {
   const resultArray = [];
+  startString = startString.toLowerCase();
   if (startString !== '' && typeof startString === 'string' && set instanceof Set) {
     set.forEach((item) => {
       if (item.startsWith(startString)) {
