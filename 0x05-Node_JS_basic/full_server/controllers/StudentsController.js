88740c86acd2de03fc3b1dcdd94cd7df376264f8
Eloagbawe <eloagbawe@gmail.com>
2023-03-21 23:33:54 +0100

    update task 8

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,38 +4,28 @@ class StudentsController {
   // constructor() { }
 
   static getAllStudents(req, res) {
-    const output = [];
+    // const output = [];
     res.status(200);
-    output.push('This is the list of our students');
+    res.write('This is the list of our students\n');
+    // output.push('This is the list of our students');
     readDatabase(process.argv[2]).then((data) => {
       const keys = Object.keys(data).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-      keys.forEach((key) => {
-        output.push(`Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`);
+      // keys.forEach((key) => {
+      //  output.push(`Number of students in ${key}: ${data[key].length}. List: ${data[key]
+      //  .join(', ')}`);
+      // });
+      keys.forEach((key, index, keys) => {
+        res.write(`Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`);
+        if (index !== keys.length - 1) {
+          res.write('\n');
+        }
       });
-      res.end(output.join('\n'));
+      res.end();
+      // res.end(output.join('\n'));
     }).catch(() => {
       res.status(500).end('Cannot load the database');
     });
   }
-  // static getAllStudents(req, res) {
-  //   res.status(200);
-  //   res.write('This is the list of our students\n');
-  //   readDatabase(process.argv[2]).then((data) => {
-  //     const keys = Object.keys(data).sort((a, b) =>
-  // a.toLowerCase().localeCompare(b.toLowerCase()));
-  //     keys.forEach((key, index, keys) => {
-  //       res.write(
-  //         `Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`
-  //       );
-  //       if (index !== keys.length - 1) {
-  //         res.write('\n');
-  //       }
-  //     });
-  //     res.end();
-  //   }).catch(() => {
-  //     res.status(500).end('Cannot load the database');
-  //   });
-  // }
 
   static getAllStudentsByMajor(req, res) {
     res.status(200);
