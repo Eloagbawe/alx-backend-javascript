61daad1f4f21307add754a13a08ee2306a720539
Eloagbawe <eloagbawe@gmail.com>
2023-03-21 23:07:07 +0100

    update task 8

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,19 +4,17 @@ class StudentsController {
   // constructor() { }
 
   static getAllStudents(req, res) {
+    const output = [];
     res.status(200);
-    res.write('This is the list of our students\n');
+    output.push('This is the list of our students');
     readDatabase(process.argv[2]).then((data) => {
-      const keys = Object.keys(data).sort();
-      keys.forEach((key, index, keys) => {
-        res.write(
+      const keys = Object.keys(data).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+      keys.forEach((key) => {
+        output.push(
           `Number of students in ${key}: ${data[key].length}. List: ${data[key].join(', ')}`,
         );
-        if (index !== keys.length - 1) {
-          res.write('\n');
-        }
       });
-      res.end();
+      res.end(output.join('\n'));
     }).catch(() => {
       res.status(500).end('Cannot load the database');
     });
