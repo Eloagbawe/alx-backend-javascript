2fc2ff7f10775d8978b59a2229c3193d7178628a
Eloagbawe <eloagbawe@gmail.com>
2023-03-23 18:49:36 +0100

    update task 8

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -2,12 +2,11 @@
 const express = require('express');
 
 const port = 7865;
-const hostname = '127.0.0.1';
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
-app.listen(port, hostname, () => {
+app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,19 +3,20 @@ const request = require('request');
 
 
 describe("api index page", function() {
-  it("test status code", function() {
-    request.get('http://localhost:7685/', (err, res, body) => {
+  it("test status code", function(done) {
+    request.get('http://localhost:7865/', (err, res, body) => {
       if (res) {
-        expect(res?.statusCode).to.equal(200);
+        expect(res.statusCode).to.equal(200);
+        done();
       }
     });
   });
-  it("test correct output", function() {
-    request.get('http://localhost:7685/', (err, res, body) => {
+  it("test correct output", function(done) {
+    request.get('http://localhost:7865/', (err, res, body) => {
       if (body) {
-        expect(body?.text).to.equal('Welcome to the payment system');
+        expect(body).to.equal('Welcome to the payment system');
+        done();
       }
     });
   });
 });
-  
