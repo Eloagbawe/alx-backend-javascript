a091ce4deb7aea3eb4bdfeddf9c7209a6bbd8204
Eloagbawe <eloagbawe@gmail.com>
2023-03-23 17:23:20 +0100

    add solution to task 7

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,7 @@
 function getPaymentTokenFromAPI(success) {
   if (success) {
     return new Promise((resolve, reject) => {
-      resolve({data: 'Successful response from the API'})
+      resolve({data: 'Successful response from the API'});
     })
   }
 };
