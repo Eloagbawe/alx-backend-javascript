9f8c068f4c5cf77d334c8a88f5b875ba6e5c395f
Eloagbawe <eloagbawe@gmail.com>
2023-03-23 18:32:54 +0100

    update api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -2,11 +2,12 @@
 const express = require('express');
 
 const port = 7865;
+const hostname = '127.0.0.1';
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`API available on localhost port ${port}`);
 });
