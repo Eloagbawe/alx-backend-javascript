bd615b39797babcda716d4a7d8ad45b4ef67d9ef
Eloagbawe <eloagbawe@gmail.com>
2023-03-23 18:28:53 +0100

    update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,14 +4,14 @@ const request = require('request');
 
 describe("api index page", function() {
   it("test status code", function() {
-    request.get('http://localhost:7685', (err, res, body) => {
+    request.get('http://localhost:7685/', (err, res, body) => {
       if (res) {
         expect(res?.statusCode).to.equal(200);
       }
     });
   });
   it("test correct output", function() {
-    request.get('http://localhost:7685', (err, res, body) => {
+    request.get('http://localhost:7685/', (err, res, body) => {
       if (body) {
         expect(body?.text).to.equal('Welcome to the payment system');
       }
