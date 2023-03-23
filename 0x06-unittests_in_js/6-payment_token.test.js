c8d4c3b2f53b4cdd4a4b78d4fbb13bdaa94c66b7
Eloagbawe <eloagbawe@gmail.com>
2023-03-23 17:02:48 +0100

    update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -7,6 +7,7 @@ describe("getPaymentTokenFromAPI", function() {
   it("test successful response from an API", function(done) {
       getPaymentTokenFromAPI(true).then((data) => {
         expect(data.data).to.equal('Successful response from the API');
+        expect(data).to.deep.equal({data: 'Successful response from the API'});
         done();
       }).catch(done)
   });
